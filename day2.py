c68cc8ac10b500f7909e15664f77c3d427bc6a82
Ibrah1mK <86393093+Ibrah1mK@users.noreply.github.com>
2021-06-25 06:41:17 +0200

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,44 @@
+import csv
+
+#part1
+with open('passwords.csv') as data:
+    reader = csv.reader(data, delimiter=' ')
+
+    validCount = 0
+    for row in reader:
+        quota, letter, pw = row[0], row[1][0], row[2]
+
+        i = quota.index('-')
+        lower = int(quota[:i])
+        upper = int(quota[i+1:])
+
+        count = 0
+        for char in pw:
+            if char == letter:
+                count += 1
+
+        if count >= lower and count <= upper:
+            validCount += 1
+
+print(validCount)
+
+#part2
+with open('passwords.csv') as data:
+    reader = csv.reader(data, delimiter=' ')
+
+    validCount = 0
+    for row in reader:
+        quota, letter, pw = row[0], row[1][0], row[2]
+
+        i = quota.index('-')
+        lower = int(quota[:i])
+        upper = int(quota[i+1:])
+
+        count = 0
+        first = pw[lower-1] == letter
+        last = pw[upper-1] == letter
+
+        if (first and not last) or (last and not first):
+            validCount += 1
+
+print(validCount)
